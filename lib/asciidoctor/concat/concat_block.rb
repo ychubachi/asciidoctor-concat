f675073f282502a1351fb21e9a9aea6f0f960180
Yoshihide Chubachi <yoshi@chubachi.net>
2019-09-11 15:15:32 +0900

    concatenates all lines in a paragraph.

--- lib/asciidoctor/concat/concat_block.rb
+++ lib/asciidoctor/concat/concat_block.rb
@@ -4,20 +4,15 @@ require 'asciidoctor/extensions'
 module Asciidoctor
   module Concat
     class Error < StandardError; end
-    # Your code goes here...
     class ConcatBlock < Asciidoctor::Extensions::BlockProcessor
-      PeriodRx = /\.(?= |$)/
-    
       use_dsl
     
-      named :shout
+      named :no_space
       on_context :paragraph
-      name_positional_attributes 'vol'
       parse_content_as :simple
     
       def process parent, reader, attrs
-          volume = ((attrs.delete 'vol') || 1).to_i
-          create_paragraph parent, (reader.lines.map {|l| l.upcase.gsub PeriodRx, '!' * volume }), attrs
+          create_paragraph parent, (reader.lines.inject {|r, l| r + l}), attrs
       end
     end
   end
