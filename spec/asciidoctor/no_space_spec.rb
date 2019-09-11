38901ea02ed0893ceec312f622f3cb804f14aded
Yoshihide Chubachi <yoshi@chubachi.net>
2019-09-11 16:23:09 +0900

    Update rspecs

--- spec/asciidoctor/no_space_spec.rb
+++ spec/asciidoctor/no_space_spec.rb
@@ -3,16 +3,49 @@ require 'asciidoctor/no_space'
 require 'asciidoctor'
 require 'asciidoctor/extensions'
 
+input1 = <<EOS
+今日の天気は
+晴れでしょう．
+It is
+fine today.
+EOS
+
+output1 = <<EOS
+<div class="paragraph">
+<p>今日の天気は
+晴れでしょう．
+It is
+fine today.</p>
+</div>
+EOS
+
+input2 = <<EOS
+[no_space]
+:hoge:
+今日の天気は
+晴れでしょう．
+It is
+fine today.
+EOS
+
+output2 = <<EOS
+<div class="paragraph">
+<p>今日の天気は晴れでしょう．It isfine today.</p>
+</div>
+EOS
+
 RSpec.describe Asciidoctor::NoSpace do
   it "has a version number" do
     expect(Asciidoctor::NoSpace::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  it "convines multiple lines without LF characters" do
+
     Asciidoctor::Extensions.register do
       block  Asciidoctor::NoSpace::NoSpaceBlock
     end
     
-    Asciidoctor.convert_file 'sample-with-shout-block.adoc', :safe => :safe
+    expect((Asciidoctor.convert input1) + "\n").to eq output1
+    expect((Asciidoctor.convert input2) + "\n").to eq output2
   end
 end
